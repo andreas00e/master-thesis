f123cd0a3bc7b6e8988c44be455e344f0a1e82c2
andreas00e <ge86tof@mytum.de>
2025-03-11 14:06:10 +0000

    Changes in common.py

--- thesis/models/common.py
+++ thesis/models/common.py
@@ -1,126 +1,69 @@
-import math
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
-
-def get_pe(hidden_size, max_len=100):  
-    pe = torch.zeros(max_len, hidden_size)
-    position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-    div_term = torch.exp(torch.arange(0, hidden_size, 2).float() * (-torch.log(torch.tensor(10000.0)) / hidden_size))
-    pe[:, 0::2] = torch.sin(position * div_term)
-    pe[:, 1::2] = torch.cos(position * div_term)
-    pe = pe.unsqueeze(0)
-    return pe
-
-
-class SinusoidalPosEmb(nn.Module):
-    def __init__(self, dim):
-        super().__init__()
-        self.dim = dim
-
+class Clip(nn.Module): 
+    def __init__(self, stability_eps: float):
+        super().__init__() 
+        self.stability_eps = stability_eps
+    
     def forward(self, x):
-        device = x.device
-        half_dim = self.dim // 2
-        emb = math.log(10000) / (half_dim - 1)
-        emb = torch.exp(torch.arange(half_dim, device=device) * -emb)
-        emb = x[:, None] * emb[None, :]
-        emb = torch.cat((emb.sin(), emb.cos()), dim=-1)
-        return emb
+        return torch.clip(x, min=-1.0+self.stability_eps, max=1.0-self.stability_eps)
 
-
-class ResBottleneck(nn.Module):
-    def __init__(self, hidden_size, norm=True) -> None:
+class ParametrizedSofmax(nn.Module):
+    def __init__(self, softmax_temp: float, softmax_temp_min: float, joint_log_softmax_temp: float, stability_eps: float):
         super().__init__()
-        self.norm = norm
-
-        self.down = nn.Linear(hidden_size, hidden_size // 4)
-        self.activation = nn.LeakyReLU()
-        self.up = nn.Linear(hidden_size // 4, hidden_size)
+        self.softmax_temp = softmax_temp
+        self.softmax_temp_min = softmax_temp_min
+        self.joint_log_softmax_temp = joint_log_softmax_temp
+        self.stability_eps = stability_eps
         
-        if self.norm:
-            self.ln = nn.LayerNorm(hidden_size)
-    
-    def forward(self, x):
-        y = self.up(self.activation(self.down(x))) + x
-        if self.norm:
-            return self.ln(y)
-        else:
-            return y
+    def forward(self, x): 
+        nom = torch.exp(x / (torch.exp(self.joint_log_softmax_temp) + self.softmax_temp_min))
+        dom = torch.sum(nom, dim=-1, keepdim=True) + self.stability_eps
+        return  nom / dom
 
-class ImageAdapter(nn.Module):
-    def __init__(self, in_dim, out_dim) -> None:
-        super().__init__()
+class JointAttentionEncoder(nn.Module): 
+    def __init__(self, desc_in_size: int, desc_out_size: int,
+                 state_in_size: int, state_out_size: int, 
+                 stability_eps: float, softmax_temp: float, softmax_temp_min: float, 
+                 *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.desc_in_size = desc_in_size
+        self.desc_out_size = desc_out_size
+        self.state_in_size = state_in_size 
+        self.state_out_size = state_out_size 
 
-        self.down = nn.Linear(in_dim, in_dim // 4)
-        self.activation = nn.LeakyReLU()
-        self.up = nn.Linear(in_dim // 4, in_dim)
+        self.softmax_temp = softmax_temp
+        self.softmax_temp_min = softmax_temp_min
+        self.joint_log_softmax_temp = nn.Parameter(
+            data=torch.log(torch.tensor(self.softmax_temp-self.softmax_temp_min)), requires_grad=True)
+        self.stability_eps = stability_eps
+
+        self.joint_desc_encoder = nn.Sequential(
+            nn.Linear(self.desc_in_size, self.desc_out_size), 
+            nn.LayerNorm(desc_out_size), 
+            nn.ELU(),
+            nn.Linear(self.desc_out_size, self.desc_out_size), 
+            nn.Tanh(), 
+            Clip(self.stability_eps), 
+            ParametrizedSofmax(self.softmax_temp, self.softmax_temp_min, self.joint_log_softmax_temp, self.stability_eps)
+            )
         
-        self.out_linear = nn.Linear(in_dim, out_dim)
+        self.joint_state_encoder = nn.Sequential(
+            nn.Linear(self.state_in_size, self.state_out_size), 
+            nn.ELU()
+            )
     
-    def forward(self, x):
-        y = self.up(self.activation(self.down(x))) + x
-        return self.out_linear(y)
+    def forward(self, joint_desc, joint_state): 
+        joint_desc_emb = self.joint_desc_encoder(joint_desc)
+        joint_state_emb = self.joint_state_encoder(joint_state)
 
+        dynamic_joint_state_mask = torch.repeat_interleave(
+            torch.unsqueeze(joint_desc_emb, dim=-1), repeats=joint_state_emb.shape[-1], dim=-1)
+        masked_dynamic_joint_state = dynamic_joint_state_mask * torch.unsqueeze(input=joint_state_emb, dim=-2)
+        masked_dynamic_joint_state = torch.reshape(masked_dynamic_joint_state, shape=(masked_dynamic_joint_state.shape[:-2]+(masked_dynamic_joint_state.shape[-2] * masked_dynamic_joint_state.shape[-1],)))
+        # masked_dynamic_joint_state = torch.reshape(
+        #     input=masked_dynamic_joint_state, shape=(masked_dynamic_joint_state.shape[:-2] + (masked_dynamic_joint_state.shape[-2] * masked_dynamic_joint_state[-1], )))
+        dynamic_joint_latent = torch.sum(masked_dynamic_joint_state, dim=-2)
 
-class WrappedTransformerEncoder(nn.Module):
-    def __init__(
-        self,
-        hidden_size: int,
-        n_heads: int,
-        dropout: int,
-        n_layers: int = None,
-        n_encoder_layers: int = None,
-        **kwargs
-    ):
-        super().__init__()
-        if n_encoder_layers is not None:
-            n_layers = n_encoder_layers
-        self.encoder = nn.TransformerEncoder(
-            encoder_layer=nn.TransformerEncoderLayer(
-                d_model=hidden_size,
-                nhead=n_heads,
-                dim_feedforward=hidden_size*4,
-                dropout=dropout,
-                activation=F.gelu,
-                batch_first=True,
-                norm_first=True,
-            ),
-            num_layers=n_layers,
-            enable_nested_tensor=False
-        )
-        self.ln = nn.LayerNorm(hidden_size)
-    
-    def forward(self, embeddings):
-        return self.ln(self.encoder(embeddings))
-
-
-class WrappedTransformerDecoder(nn.Module):
-    def __init__(
-        self,
-        hidden_size: int,
-        n_heads: int,
-        dropout: int,
-        n_layers: int = None,
-        n_decoder_layers: int = None,
-        **kwargs
-    ):
-        super().__init__()
-        if n_decoder_layers is not None:
-            n_layers = n_decoder_layers
-        self.decoder = nn.TransformerDecoder(
-            decoder_layer=nn.TransformerDecoderLayer(
-                d_model=hidden_size,
-                nhead=n_heads,
-                dim_feedforward=hidden_size*4,
-                dropout=dropout,
-                activation=F.gelu,
-                batch_first=True,
-                norm_first=True
-            ),
-            num_layers=n_layers
-        )
-        self.ln = nn.LayerNorm(hidden_size)
-    
-    def forward(self, tgt, memory):
-        return self.ln(self.decoder(tgt, memory))
+        return dynamic_joint_latent
