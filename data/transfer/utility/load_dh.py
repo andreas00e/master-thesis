158d4a8feebcdbca04840ff04baf73b44853e1d7
andreas00e <andreas.ehrensberger@tum.de>
2026-07-20 23:49:49 +0800

    Keep pushing

---
+++ data/transfer/utility/load_dh.py
@@ -0,0 +1,18 @@
+import hydra 
+from omegaconf import DictConfig, OmegaConf
+
+import torch
+
+config_path = "../dh_conf"
+config_name = "dh_params"
+
+@hydra.main(config_path=config_path, config_name=config_name, version_base=None)
+def main(cfg: DictConfig) -> None: 
+    
+    cfg_dict = {
+        key: torch.stack([torch.tensor(v, dtype=torch.float32) for v in inner.values()]).T
+        for key, inner in cfg.items()
+    }
+        
+if __name__ == "__main__": 
+    main()
