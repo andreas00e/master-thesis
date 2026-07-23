584938f98a8defa1a35d5a3171f1437306051986
andreas00e <andreas.ehrensberger@tum.de>
2026-07-23 21:05:57 +0800

    Fixed some things

--- skill_discovery.py
+++ skill_discovery.py
@@ -1,30 +1,20 @@
-import hydra 
-from datetime import datetime 
-from zoneinfo import ZoneInfo
+import hydra
+from hydra.utils import instantiate
 
 import lightning as pl
-from lightning.pytorch.loggers import WandbLogger
-
-from models.discover.discover import SkillDiscovery
-from data.discover.datamodule import MimicGenRobotDataModule
 
 
 @hydra.main(config_path="cfgs/", config_name="skill_discovery.yaml", version_base=None)
 def main(cfg): 
     pl.seed_everything(cfg.seed)
 
-    datamodule = MimicGenRobotDataModule(**cfg.data.datamodule)
-    model = SkillDiscovery(**cfg.model)
-    
-    cfg.logger.verion = datetime.now(ZoneInfo("Europe/Berlin")).strftime("%Y_%m_%d_%H_%M")
-    logger = WandbLogger(**cfg.logger)
+    datamodule = instantiate(cfg.data.datamodule)
+    model = instantiate(cfg.model)
+    logger = instantiate(cfg.logger)
 
-    trainer = pl.Trainer(
-        logger=logger, 
-        **cfg.trainer
-        )
-    _ = trainer.test(model=model, datamodule=datamodule)
-    
+    trainer = pl.Trainer(logger=logger, **cfg.trainer)
+    _  = trainer.test(model=model, datamodule=datamodule)
+
 
 if __name__ == "__main__": 
     main()
