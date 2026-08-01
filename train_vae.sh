35fc4ae44d95356a7110c8e1acd0123effadd859
andreas00e <andreas.ehrensberger@tum.de>
2026-08-02 03:17:17 +0800

    HAVE YOU EVER SEEN SO MANY CHANGES

--- train_vae.sh
+++
@@ -1,3 +0,0 @@
-python3 thesis/train_vae.py --config_path thesis/configs/train_vae.yaml \
-                            --data_dir ~/ehrensberger/mimicgen/datasets/core
-                                                          
