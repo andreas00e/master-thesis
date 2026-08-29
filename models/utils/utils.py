d64ec90baeaba991eba858505f41ebdaf466e518
andreas00e <andreas.ehrensberger@tum.de>
2026-08-29 18:12:30 +0200

    Updates in positional encoding

--- models/utils/utils.py
+++ models/utils/utils.py
@@ -1,4 +1,4 @@
-# ADJUSTED FROM: 
+# !!! ADJUSTED FROM !!!: 
 # https://discuss.pytorch.org/t/how-to-modify-the-positional-encoding-in-torch-nn-transformer/104308
 
 import math
@@ -23,22 +23,23 @@ class PE(nn.Module):
         div_term = torch.exp(torch.arange(0, self.d_model, 2).float() * (-math.log(10000.0) / self.d_model)) # [d_model / 2]
         pe[:, 0::2] = torch.sin(position * div_term)  
         pe[:, 1::2] = torch.cos(position * div_term) 
-        self.register_buffer("pe", pe)
+        self.register_buffer("pe", pe, persistent=True)
         
     def forward(
         self, 
         x: TensorType["batch*chunk", "1+window", "d_model"], 
         seq_idxs: Optional[TensorType["batch", "chunk", "window"]]=None
         ) -> TensorType["batch*chunk", "1+window", "d_model"]:
-                        
+                
         if isinstance(seq_idxs, torch.Tensor): 
             pe_idxs = torch.min(seq_idxs, dim=-1, keepdim=True).values # [batch, chunks, 1] 
             pe_idxs = torch.cat(tensors=(pe_idxs, seq_idxs+1), dim=-1) # [batch, chunks, 1+window]
             pe_idxs = pe_idxs.view(-1, x.shape[1]) # [batch*chunk, 1+window]
         else:  
-            pe_idxs = torch.arange(x.shape[1], dtype=x.dtype, device=x.device) # [1+window]
+            pe_idxs = torch.arange(x.shape[1], dtype=torch.long, device=x.device) # [1+window]
             pe_idxs = pe_idxs[None, :].expand(x.shape[0], -1) # [batch*chunk, 1+window]
-
+            
+        pe_idxs = torch.clamp(pe_idxs, max=self.max_len - 1)
         pe = self.pe[pe_idxs] # [batch*chunk, 1+window, d_model]
     
         return x + pe # [batch*chunk, 1+window, d_model]
