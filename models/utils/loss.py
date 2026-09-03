11d84b0a603e28e3acf150b0c7c705467d1ac47d
andreas00e <andreas.ehrensberger@tum.de>
2026-09-03 14:17:00 +0200

    Implemented soft optimal uncertainty weighting of losses

--- models/utils/loss.py
+++ models/utils/loss.py
@@ -2,8 +2,10 @@ from typing import List
 
 import torch 
 import torch.nn as nn 
+import torch.nn.functional as F 
 
-class UncertaintyLossWraper(nn.Module): 
+# https://arxiv.org/abs/1705.07115
+class UncertaintyWeighting(nn.Module): 
     def __init__(self, num_modalities: int=2) -> None:
         super().__init__()
         
@@ -11,11 +13,27 @@ class UncertaintyLossWraper(nn.Module):
         self.log_vars = nn.Parameter(torch.zeros(self.num_modalities))
         
     def  forward(self, losses: List[torch.Tensor]) -> torch.Tensor: 
-        losses = torch.stack(losses, dim=0) # [num_modalities, 1]
+        losses = torch.stack(losses) # [num_modalities, 1]
         
         precisions = torch.exp(-self.log_vars) # [num_modalities, 1]
         weighted_losses = 1/2 * precisions * losses + 1/2 * self.log_vars # [num_modalities, 1]
         
-        loss = torch.sum(weighted_losses, dim=0) # []
+        loss = torch.sum(weighted_losses) # []
         
         return loss 
+  
+# https://arxiv.org/abs/2408.07985  
+class SoftOptimalUncertaintyWeighting(nn.Module): 
+    def __init__(self, temperature: float=1.0) -> None:
+        super().__init__()
+        self.temperature = temperature
+    
+    def forward(self, losses: List[torch.Tensor]) -> torch.Tensor: 
+        losses = torch.stack(losses) # [num_modalities]
+        
+        precisions = torch.exp(losses.clone().detach() / self.temperature) # [num_modalities]
+        weighted_losses = F.softmax(precisions, dim=0) * losses # [num_modalities]
+    
+        loss = torch.sum(weighted_losses)
+        
+        return loss
