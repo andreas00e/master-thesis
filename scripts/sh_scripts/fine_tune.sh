40f4d00ac2798f686f9a365479e9caf88ce277f8
Andreas Ehrensberger <andreas.ehrensberger@tum.de>
2026-09-12 17:29:55 +0200

    Debugged sbatch fine_tune script

--- scripts/sh_scripts/fine_tune.sh
+++ scripts/sh_scripts/fine_tune.sh
@@ -3,11 +3,17 @@
 #SBATCH --output=outputs/fine_tune/output/training_%j.log
 #SBATCH --error=outputs/fine_tune/error/training_%j.err
 #SBATCH --time=05:00:00
-#SBATCH --partition=lrz-dgx-1-p100x8 
-#SBATCH --nodelist=dgx-001
+#SBATCH --partition=lrz-v100x2
+#SBATCH --nodelist=gpu-005
 #SBATCH --gres=gpu:1
 #SBATCH --mem=32GB
 
-source /dss/dssfs04/lwp-dss-0002/pn36ce/pn36ce-dss-0000/ehrensberger/sim/bin/activate
+mkdir -p outputs/fine_tune/output outputs/fine_tune/error
 
-python3 ../py_scripts/fine_tune.py
+source /dss/dssfs04/lwp-dss-0002/pn36ce/pn36ce-dss-0000/ehrensberger/sim/bin/activate
+cd /dss/dssfs04/lwp-dss-0002/pn36ce/pn36ce-dss-0000/ehrensberger/master-thesis
+
+export HYDRA_FULL_ERROR=1
+export PYTHONPATH="$PWD:$PYTHONPATH"
+
+python3 scripts/py_scripts/fine_tune.py
