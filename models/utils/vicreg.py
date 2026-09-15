e3f20fac057affdf1866c4e670d747e2f7c62842
Andreas Ehrensberger <andreas.ehrensberger@tum.de>
2026-09-15 09:01:26 +0200

    Corrected VICReg implementation

--- models/utils/vicreg.py
+++ models/utils/vicreg.py
@@ -31,21 +31,21 @@ class VICReg(nn.Module):
         self.nu = nu
         self.gamma = gamma
         self.eps = eps
+    
+    def _invariance_loss(self, x: TensorType["n", "d"], x_: TensorType["n", "d"]) -> torch.Tensor: 
+        n = x.shape[0]
         
-    def _variance_loss(self, x: TensorType["n", "d"]) -> torch.Tensor: 
-        var = torch.var(x, dim=0) # [d]
-        std = torch.sqrt(var + self.eps) # [d]
-        out = torch.mean(F.relu(self.gamma - std)) # []
-        
-        if self.logger is not None: 
-            if self._global_step % self.log_every_n_steps == 0: 
-                self.logger.experiment.log({
-                    "train/var_histogram": wandb.Histogram(var.detach().cpu().numpy())
-                })
-        
-        self._global_step += 1 
+        diff = torch.sum((x - x_) ** 2, dim=1) # [n]
+        out = (1 / n) * torch.sum(diff) # []
         
         return out
+        
+    def _variance_loss(self, x: TensorType["n", "d"]) -> torch.Tensor: 
+        var = torch.var(x, dim=0, unbiased=True) # [d]
+        std = torch.sqrt(var + self.eps) # [d]
+        out = torch.mean(F.relu(self.gamma - std)) # []
+
+        return out, var
     
     def _covariance_loss(self, x: TensorType["n", "d"]) -> torch.Tensor: 
         n, d = x.shape 
@@ -60,15 +60,27 @@ class VICReg(nn.Module):
         return out 
     
     def forward(self, z: TensorType["n", "d"], z_: TensorType["n", "d"]) -> torch.Tensor: 
-        inv_loss = F.mse_loss(z, z_) # []
-        var_loss = self._variance_loss(z) + self._variance_loss(z_) # []
+        inv_loss = self._invariance_loss(z, z_) # []
+        
+        var_loss_z , var= self._variance_loss(z)
+        var_loss_z_, _ = self._variance_loss(z_)
+        var_loss = var_loss_z + var_loss_z_ # []
+        
         cov_loss = self._covariance_loss(z) + self._covariance_loss(z_) # []
         tot_loss = self.lambda_ * inv_loss + self.mu * var_loss + self.nu * cov_loss # []
         
+        if self.logger is not None: 
+            if self._global_step % self.log_every_n_steps == 0: 
+                self.logger.experiment.log({
+                    "train/var_histogram": wandb.Histogram(var.detach().cpu().numpy())
+                })
+
+        self._global_step += 1 
+                
         logs_ = {
             "inv_loss": inv_loss.detach(), 
             "var_loss": var_loss.detach(), 
             "cov_loss": cov_loss.detach() 
         }
-     
+             
         return tot_loss, logs_
